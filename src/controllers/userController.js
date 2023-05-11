ea05ee90105e1a6a57adc200f7a58401d29ecb40
Pallavi-s-r <pallavisingh.sgrl9@gmail.com>
2023-05-11 17:01:37 +0530

    auth1

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,14 +1,9 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
-
 const createUser = async function (req, res) {
-  //You can name the req, res objects anything.
-  //but the first parameter is always the request 
-  //the second parameter is always the response
   let data = req.body;
   let savedData = await userModel.create(data);
-  console.log(req.newAtribute);
   res.send({ msg: savedData });
 };
 
@@ -21,51 +16,13 @@ const loginUser = async function (req, res) {
     return res.send({
       status: false,
       msg: "username or the password is not corerct",
-    },{bufferTimeOutMs: 30000});
-
-  // Once the login is successful, create the jwt token with sign function
-  // Sign function has 2 inputs:
-  // Input 1 is the payload or the object containing data to be set in token
-  // The decision about what data to put in token depends on the business requirement
-  // Input 2 is the secret (This is basically a fixed value only set at the server. This value should be hard to guess)
-  // The same secret will be used to decode tokens 
-  let token = jwt.sign(
-    {
-      userId: user._id.toString(),
-      batch: "techtnetium",
-      role: "Trainee",
-    },
-    "functionup-technetium- trainee-very-very-secret-key"
-  );
+    });
+  let token = jwt.sign({userId: user._id.toString()},"technetium-batch");
   res.setHeader("x-auth-token", token);
   res.send({ status: true, token: token });
 };
 
 const getUserData = async function (req, res) {
-  let token = req.headers["x-Auth-token"];
-//   if (!token)  = req.headers["x-auth-token"];
-
-//   //If no token is present in the request header return error. This means the user is not logged in.
-  if (!token) 
-return res.send({ status: false, msg: "token must be present" });
-
-  console.log(token);
-
-//   // If a token is present then decode the token with verify function
-//   // verify takes two inputs:
-//   // Input 1 is the token to be decoded
-//   // Input 2 is the same secret with which the token was generated
-//   // Check the value of the decoded token yourself
-
-//   // Decoding requires the secret again. 
-//   // A token can only be decoded successfully if the same secret was used to create(sign) that token.
-//   // And because this token is only known to the server, it can be assumed that if a token is decoded at server then this token must have been issued by the same server in past.
-//   
-try{
-  let decodedToken = jwt.verify(token, "functionup-technetium- trainee-very-very-secret-key");
-  console.log(decodedToken)
-  if (!decodedToken)
-    return res.send({ status: false, msg: "token is invalid" });
 
   let userId = req.params.userId;
   let userDetails = await userModel.findById(userId);
@@ -73,33 +30,41 @@ try{
     return res.send({ status: false, msg: "No such user exists" });
 
   res.send({ status: true, data: userDetails });
-} catch (error) {
-  return res.status(500).json({ status: false, msg: "Internal server error" });
-}
-//   // Note: Try to see what happens if we change the secret while decoding the token
 };
 
-
-
-const updateUser = async function (req, res) {
-  // Do the same steps here:
-  // Check if the token is present
-  // Check if the token present is a valid token
-  // Return a different error message in both these cases
+const updatedUserAttribute = async function (req, res) {
 
   let userId = req.params.userId;
   let user = await userModel.findById(userId);
+  if (!user) {
+    return res.send("No such user exists");
+  }
+
+  let userAttribute = req.body;
+  let updatedUserAttribute = await userModel.findOneAndUpdate(
+    { _id: userId },
+    userAttribute,
+    {new: true}
+  );
+  res.send({ status:true , data: updatedUserAttribute });
+};
+
+const updateIsDelete = async function (req, res) {
+  const userIdInfo = req.params.userId;
+  let user = await userModel.findById(userIdInfo);
   //Return an error if no user with the given id exists in the db
   if (!user) {
     return res.send("No such user exists");
   }
 
-  let userData = req.body;
-  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
-  res.send({ status: updatedUser, data: updatedUser });
+ if(userIdInfo){
+  let updatedIsDeleted= await userModel.findOneAndUpdate({ _id: userIdInfo },{ isDeleted:true},{new:true});
+  res.send({ status: updatedIsDeleted, data: updatedIsDeleted });
+ }
 };
 
 module.exports.createUser = createUser;
-module.exports.getUserData = getUserData;
-module.exports.updateUser = updateUser;
 module.exports.loginUser = loginUser;
+module.exports.getUserData = getUserData;
+module.exports.updatedUserAttribute = updatedUserAttribute;
+module.exports.updateIsDelete = updateIsDelete;
---
+++ src/middleware/middleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const middleware1 = function (req, res, next) {
+  let token = req.headers["x-auth-token"];
+  //   if (!token) token = req.headers["x-auth-token"];
+  if (!token) return res.send({ status: false, msg: "token must be present" });
+
+  console.log(token);
+
+   jwt.verify(token, "technetium-batch",
+   (error,token)=>{
+    console.log(token,error)
+    if (!token)
+      return res.send({ status: false, msg: "token is invalid" });
+
+  });
+};
+
+
+module.exports.middleware1 = middleware1;
+module.exports.middleware2=middleware2
